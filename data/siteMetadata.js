c995c95c86a2ebdaabcbf463e9f4e5b1c62df041
Nick <nick.nguyen@contentstack.com>
2022-10-07 14:55:07 -0700

    test

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Made With Nick',
+  author: 'Nicholas Nguyen',
+  headerTitle: 'Made With Nick',
+  description: 'A view into my life and the things I am pursuing.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
