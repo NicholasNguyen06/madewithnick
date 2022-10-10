435540ef85a3caf8353572f93dc46bc4a460e240
Nick <nick.nguyen@contentstack.com>
2022-10-10 15:32:12 -0700

    Logo fix

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -18,7 +18,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Image src={PNGLogo} width={200} height={200} />
+                  <Image className="object-scale-down" src={PNGLogo} width={125} height={125} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
