dd688a23e6292e123fb1263bc7dbfac0ff11bbc0
Nick <nick.nguyen@contentstack.com>
2022-10-10 15:24:44 -0700

    Logo & new post

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,8 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '@/data/logo.png'
+import PNGLogo from '../data/logo.png'
+import Image from 'next/image'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -16,11 +18,11 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Image src={PNGLogo} width={200} height={200} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
+                    {siteMetadata.title}
                   </div>
                 ) : (
                   siteMetadata.headerTitle
